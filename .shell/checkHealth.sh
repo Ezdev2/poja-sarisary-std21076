eaa78c970601d52d19e377881f04e314709f502a
ezdev2 <fansoaezra@gmail.com>
2024-02-22 14:37:28 +0300

    fix checkHealth.sh #2

--- .shell/checkHealth.sh
+++ .shell/checkHealth.sh
@@ -1,16 +1,4 @@
-# sudo apt-get install jq
-# export API_URL_SSM="`aws ssm get-parameter --name /poja-sarisary-std21076/$1/api/url`"
-# export API_URL=`echo $API_URL_SSM | jq -r '.Parameter.Value'`
-# curl --fail "$API_URL$2"
-
-sudo apt-get install -y jq
-
-echo "Récupération de l'URL depuis AWS SSM..."
+sudo apt-get install jq
 export API_URL_SSM="`aws ssm get-parameter --name /poja-sarisary-std21076/$1/api/url`"
-echo "URL brute récupérée depuis AWS SSM : $API_URL_SSM"
-
 export API_URL=`echo $API_URL_SSM | jq -r '.Parameter.Value'`
-echo "URL formatée : $API_URL"
-
-echo "Exécution de la requête curl : $API_URL$2"
 curl --fail "$API_URL$2"
